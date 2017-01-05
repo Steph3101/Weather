4853fa32414889671443c78fb6191b3af7d8c9c3
Stéphane AZZOPARDI <contact@stephane-azzopardi.fr>
2017-01-05 09:49:37 +0100

    Create weather files

---
+++ Weather/Forecast.swift
@@ -0,0 +1,15 @@
+//
+//  Forecast.swift
+//  Weather
+//
+//  Created by Stéphane AZZOPARDI on 04/01/2017.
+//  Copyright © 2017 Stéhane Azzopardi AE. All rights reserved.
+//
+
+import UIKit
+
+class Forecast: NSObject {
+
+    var city: String?
+    var day: String?
+}
---
+++ Weather/WeatherViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherViewModel.swift
+//  Weather
+//
+//  Created by Stéphane AZZOPARDI on 05/01/2017.
+//  Copyright © 2017 Stéhane Azzopardi AE. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewModel: NSObject {
+
+}
